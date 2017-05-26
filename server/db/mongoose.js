5d9148223bec591fc1eda677d9eb5bbfd63204af
Karthik Pandurangan <kpandurangan@FORZACONSULTING.local>
2017-05-26 21:35:44 +0200

    Prepare for Heroku 1.1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoAppMongoose');
+
+// Have to see mongo DB appropriately if this on Heroku
+if(process.env.PORT){
+    mongoose.connect('mongodb://karthik:Forza123!@ds151631.mlab.com:51631/todoapp1');
+}
+else{
+    mongoose.connect('mongodb://localhost:27017/TodoAppMongoose');
+}
+
 
 module.exports = {mongoose};
